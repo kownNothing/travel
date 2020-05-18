d248cf3deb3b774b75ef7c9ff96d469ed0d5d6a3
rock-narwhal <1987747418@qq.com>
2020-05-18 09:27:58 +0800

    添加BaseServlet

---
+++ src/main/java/travel/web/servlet/base/BaseServlet.java
@@ -0,0 +1,44 @@
+package travel.web.servlet.base;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+public class BaseServlet extends HttpServlet {
+    /**
+     * 根据请求的uri(/user/add)执行对应的servlet中的同名的方法
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String URI=req.getRequestURI();
+        String methodName=URI.substring(URI.lastIndexOf('/')+1);
+
+        try{
+            Method method=this.getClass().getMethod(methodName,HttpServletRequest.class,HttpServletResponse.class);
+            method.invoke(this,req,resp);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        super.service(req, resp);
+    }
+
+    public void writeValue(Object obj,HttpServletResponse resp) throws IOException {
+        resp.setContentType("application/json;charset=utf-8");
+        resp.getWriter().write(writeValueAsString(obj));
+    }
+
+    public String writeValueAsString(Object obj) throws IOException {
+        ObjectMapper mapper=new ObjectMapper();
+        return mapper.writeValueAsString(obj);
+    }
+}
